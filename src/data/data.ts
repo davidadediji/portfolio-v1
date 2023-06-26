22c2dc63859c735c646663f7515ad3d03e3d2391
davidadediji <davidadediji@gmail.com>
2023-06-26 18:04:39 +0100

    add images

--- src/data/data.ts
+++ src/data/data.ts
@@ -16,3 +16,41 @@ export const navItems = [
 		path: 'davidadeyinka-cv.pdf',
 	},
 ];
+
+export const cardDetails = [
+	{
+		imageUrl: 'About',
+		title: '#about',
+		summary: '',
+	},
+	{
+		imageUrl: 'About',
+		title: '#about',
+		summary: '',
+	},
+	{
+		imageUrl: 'About',
+		title: '#about',
+		summary: '',
+	},
+	{
+		imageUrl: 'About',
+		title: '#about',
+		summary: '',
+	},
+];
+
+export const companies = [
+	{
+		imageUrl: 'About',
+		name: '#about',
+	},
+	{
+		imageUrl: 'About',
+		name: '#about',
+	},
+	{
+		imageUrl: 'About',
+		name: '#about',
+	},
+];
